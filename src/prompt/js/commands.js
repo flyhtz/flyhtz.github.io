92bc459114db0f1ae2c7124befcf7410a77f6734
Flyhtz <dabombgamer999@gmail.com>
2022-12-26 18:47:30 +0100

    added prompt to website
    the interactive prompt has been added to the website

---
+++ src/prompt/js/commands.js
@@ -0,0 +1,66 @@
+var youtube = "https://www.youtube.com/flyhtz/";
+var password = "flyhtz";
+var instagram = "https://www.instagram.com/autisticprogrammer/";
+var github = "https://github.com/flyhtz/";
+
+whois = [
+  "<br>",
+  "Hey, I'm Flyhtz!ðŸ‘‹",
+  "I'm a software developer and kind of was a content creator.",
+  "I make websites for clients and projects for myself to grow in this coding world thing.",
+  "Currently working fulltime on this website and other projects.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/flyhtz' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/autisticprogrammer' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/flyhtz' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Flyhtz?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">flyhtz (Fz) Not A Corporation. All rights reserved.</span>',
+  "@@@@@@@@ @@@      @@@ @@@ @@@  @@@ @@@@@@@ @@@@@@@@",
+  "@@!      @@!      @@! !@@ @@!  @@@   @@!        @@!",
+  "@!!!:!   @!!       !@!@!  @!@!@!@!   @!!      @!!  ",
+  "!!:      !!:        !!:   !!:  !!!   !!:    !!:    ",
+  ":       : ::.: :   .:     :   : :    :    :.::.: : ",
+                                                     
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
